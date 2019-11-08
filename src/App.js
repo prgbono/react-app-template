114d703f46062276f6c8399948b3f41fd8a20dbc
Francisco Rios <francisco.riosgalan@keytree.es>
2019-11-08 18:46:46 +0100

    Bloque Hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParams';
 const App = () => {
   return (
     <div>
       <h1 id="something-important">React App Template</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <SearchParams />
     </div>
   );
 };
